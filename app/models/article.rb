02cad67ffef204cd3b38fe8cb333cc159026324b
Danger R.Galvez <d.rodriguez_galvez@hotmail.com>
2022-01-06 00:49:18 +0100

    Implement rich_text editor
    - Install action_text
    - Add validations to article.rb
    - Import LocalTime stamp in app.js

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,8 @@
 class Article < ApplicationRecord
+  # Validations
+    validates :title, presence: true
+    validates :body, presence: true
+
+  # Actions
+    has_rich_text :body    # This enable rich_text on the article#body
 end
